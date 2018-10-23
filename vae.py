982274fa806f94342561fc80f717f05cec8aa2a7
Peter Organisciak <organisciak@gmail.com>
2018-10-23 20:43:52 +0000

    Update VAE to better scale hidden dims

--- vae.py
+++ vae.py
@@ -6,37 +6,58 @@ from keras.losses import mse, binary_crossentropy
 from keras import backend as K
 from keras.utils import multi_gpu_model
 
-def create_vae(original_dim, intermediate_dim, latent_dim, loss_metric="CrossEntropy", optimizer="RMSProp",
-               intermediate_dim2=False, learning_rate=0.001, epsilon_std=1.):
+def create_vae(dims, loss_metric="CrossEntropy", optimizer="RMSProp",
+               intermediate_dim2=False, learning_rate=0.001, epsilon_std=1.,
+              print_summary=False):
 
     def sampling(args):
         z_mean, z_log_var = args
         epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
                                   stddev=epsilon_std)
         return z_mean + K.exp(z_log_var / 2) * epsilon
+
+    # The dims arg should be a string of format '1000-200-100-50',
+    # going from the input size to the hidden layer sizes and finally the latent
+    # dimensions
+    diml = [int(d) for d in dims.split('-')]
+    assert len(diml) > 2
+    original_dim = diml[0]
+    latent_dim = diml[-1]
+    hidden_dims = diml[1:-1]
     
     # Build Encoder
     inputs = Input(shape=(original_dim,))
-    h = Dense(intermediate_dim, activation='relu', name='HiddenLayer1')(inputs)
-    if intermediate_dim2:
-        h = Dense(intermediate_dim2, activation='relu', name='HiddenLayer2')(h)
+    
+    for i, hdim in hidden_dims:
+        layer = Dense(hdim, activation='relu', name='HiddenLayer%d' %i)
+        if i == 0:
+            h = layer(inputs)
+        else:
+            h = layer(h)
 
     z_mean = Dense(latent_dim, name='z_mean')(h)
     z_log_var = Dense(latent_dim, name='z_log_var')(h)
+    
     z = Lambda(sampling, output_shape=(latent_dim,), name='EncodedOutput')([z_mean, z_log_var])
     encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-    encoder.summary()
+    if print_summary:
+        encoder.summary()
 
     # build Decoder
     latent_inputs = Input(shape=(latent_dim,), name='DecoderInput')
-    if intermediate_dim2:
-        h2_decoded = Dense(intermediate_dim2, activation='relu', name="DecoderHLayer2")(latent_inputs)
-        h_decoded = Dense(intermediate_dim, activation='relu', name="DecoderHLayer1")(h2_decoded)
-    else:
-        h_decoded = Dense(intermediate_dim, activation='relu', name="DecoderHLayer1")(latent_inputs)
+    
+    for i, hdim in hidden_dims[::-1]:
+        j = len(hidden_dims) - i
+        layer = Dense(hdim, activation='relu', name="DecoderHLayer%d" % j)
+        if i == 0:
+            h_decoded = layer(latent_inputs)
+        else:
+            h_decoded = layer(h_decoded)
+    
     decoder_outputs = Dense(original_dim, activation='sigmoid', name="ReconstructedOutput")(h_decoded)
     decoder = Model(latent_inputs, decoder_outputs, name='decoder')
-    decoder.summary()
+    if print_summary:
+        decoder.summary()
 
     # instantiate VAE model
     # Use the reconstructed version ('z', index=2) of the input data as the output
